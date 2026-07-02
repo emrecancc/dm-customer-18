3db3fc3b337c8401cdb1e08c6f01a00480872893
emrecancc <emrecancerli55@gmail.com>
2026-07-02 23:48:45 +0300

    fix: add missing colon to get_user method

--- app/services.py
+++ app/services.py
@@ -2,5 +2,8 @@ class UserService:
     def __init__(self, db):
         self.db = db
 
-    def get_user(self, user_id)
-        return self.db.query('SELECT * FROM users WHERE id = ?', user_id)
+    def get_user(self, user_id):
+        return self.db.get_user(user_id)
+
+    def create_user(self, user_data):
+        return self.db.create_user(user_data)
