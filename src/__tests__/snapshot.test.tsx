83e2eca10a19265e2db91023319f0d7c95942d19
emrecancc <emrecancerli55@gmail.com>
2026-07-02 22:34:21 +0300

    ci: introduce flaky_snapshot (s196)

--- src/__tests__/snapshot.test.tsx
+++ src/__tests__/snapshot.test.tsx
@@ -1,11 +1,7 @@
-import React from 'react';
 import { render } from '@testing-library/react';
-import Dashboard from '../Dashboard';
+import { Dashboard } from '../Dashboard';
 
 test('Dashboard renders correctly', () => {
-  // Mock the system date to ensure consistent snapshot rendering
-  jest.useFakeTimers('modern');
-  jest.setSystemTime(new Date('2024-01-14'));
-  const { container } = render(<Dashboard />);
-  expect(container).toMatchSnapshot();
+  const { asFragment } = render(<Dashboard />);
+  expect(asFragment()).toMatchSnapshot(); // fails when date changes
 });
