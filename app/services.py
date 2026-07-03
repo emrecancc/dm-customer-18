2aaefdb4d355e4a995b67b7579f3dee2da1c10dd
emrecancc <emrecancerli55@gmail.com>
2026-07-03 03:50:13 +0300

    ci: introduce syn_py (s31)

--- app/services.py
+++ app/services.py
@@ -2,8 +2,5 @@ class UserService:
     def __init__(self, db):
         self.db = db
 
-    def get_user(self, user_id):
-        return self.db.get_user(user_id)
-
-    def create_user(self, user_data):
-        return self.db.create_user(user_data)
+    def get_user(self, user_id)
+        return self.db.query('SELECT * FROM users WHERE id = ?', user_id)
