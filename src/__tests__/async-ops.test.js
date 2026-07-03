4d7202d8bac16bb7957c87aadc70033f2b2aa553
emrecancc <emrecancerli55@gmail.com>
2026-07-03 05:47:51 +0300

    fix: update async-ops test to use Promise.all

--- src/__tests__/async-ops.test.js
+++ src/__tests__/async-ops.test.js
@@ -1,12 +1,9 @@
+const { processBatch } = require('../src/batchProcessor');
+
 describe('batch processing', () => {
-  it('processes all items', (done) => {
-    const results = [];
-    const promises = Array.from({length: 10}, (_, i) =>
-      processItem(i).then(r => results.push(r))
-    );
-    setTimeout(() => {
-      expect(results).toHaveLength(10); // race condition: may not be done
-      done();
-    }, 100);
+  test('processes all items', async () => {
+    const items = Array.from({ length: 10 }, (_, i) => i);
+    const results = await Promise.all(items.map(item => processBatch(item)));
+    expect(results).toHaveLength(10);
   });
 });
